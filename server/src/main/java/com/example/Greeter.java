8614be778a63d1f40efa312e8873688b86d5b419
onavarrf <62543459+onavarrf@users.noreply.github.com>
2020-03-26 11:29:42 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,6 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
+  /** Returns a greeting. */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
